2529cf46f4eeadd34f21aa087778ce5f05aee734
Khaibar Saadat <khybersaadat93@gmail.com>
2023-11-15 21:20:49 +0100

    Another Way Of Typing Components

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -19,3 +19,18 @@ export default function CourseGoal({ title, children }: CourseGoalProps) {
     </article>
   );
 }
+
+// Alternative way of Typing Components using FC Type with an Arrow function
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>DELETE</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
