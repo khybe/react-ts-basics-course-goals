e149f1efd8f707a23445e9f07e9b947b2a892526
Khaibar Saadat <khybersaadat93@gmail.com>
2023-11-15 23:28:11 +0100

    Passing Functions as Values - In A Type-Safe Way

--- src/App.tsx
+++ src/App.tsx
@@ -25,13 +25,17 @@ export default function App() {
     });
   };
 
+  function HandleDeleteGoal(id: number) {
+    setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== id));
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
       <button onClick={handleAddGoal}>Add Goal</button>
-      <CourseGoalList goals={goals} />
+      <CourseGoalList goals={goals} onDeleteGoal={HandleDeleteGoal} />
     </main>
   );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,21 +1,28 @@
 import { type ReactNode } from "react";
 
 interface CourseGoalProps {
+  id: number;
   title: string;
   children: ReactNode;
+  onDelete: (id: number) => void;
 }
 
 // Alternative way, using 'PropsWithChildren' to define our Props Types
 // type CourseGoalProps = PropsWithChildren<{ title: string }>;
 
-export default function CourseGoal({ title, children }: CourseGoalProps) {
+export default function CourseGoal({
+  id,
+  title,
+  children,
+  onDelete,
+}: CourseGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
         {children}
       </div>
-      <button>DELETE</button>
+      <button onClick={() => onDelete(id)}>DELETE</button>
     </article>
   );
 }
