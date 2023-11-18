d0ca2cd1f670546554c25294648dc8de4b8a1032
Khaibar Saadat <khybersaadat93@gmail.com>
2023-11-18 19:00:30 +0100

    Building a More Dynamic & Flexible Component 'InfoBox' and rendering it conditionaly

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,8 @@
-import CourseGoal from "./CourseGoal.tsx";
+import { type ReactNode } from "react";
 import { type CourseGoal as CGoal } from "../App.tsx";
+
+import CourseGoal from "./CourseGoal.tsx";
+import InfoBox from "./InfoBox.tsx";
 
 // Define the props for the CourseGoalList component
 type CourseGoalListProps = {
@@ -14,18 +17,39 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {/* Map through the goals array and render CourseGoal component for each goal */}
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          {/* CourseGoal component with TypeScript types for props */}
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            {/* Render the description of the goal */}
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {/* Map through the goals array and render CourseGoal component for each goal */}
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            {/* CourseGoal component with TypeScript types for props */}
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              {/* Render the description of the goal */}
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
